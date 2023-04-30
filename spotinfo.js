06c741d6acfa67aa2ad0148f2bafad3d056cbcd1
fluffy <69675847+fluffy-git@users.noreply.github.com>
2023-04-30 19:46:45 +0200

    initial release

---
+++ spotinfo.js
@@ -0,0 +1,86 @@
+var serviceHost = "https://spotify-listening-to-worker.eduardgavrila475204.workers.dev/"; // worker url
+var spotifyUser = "fluffy"; //username, doesnt need to be the actual name of the user
+
+var songData, progressSeconds, totalSeconds, progressInterval;
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+function updatePlayer() {
+	fetch(`${serviceHost}/get-now-playing`)
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.hasOwnProperty("ERROR")) {
+				document.getElementById(
+					"player-song"
+				).innerHTML = `${spotifyUser} isn't playing anything.`;
+				document.getElementById("player-artist").innerHTML = "  ";
+				document.getElementById("player-pt").setAttribute("style", "display: none;");
+
+				sleep(500);
+				updatePlayer();
+			}
+
+			songData = data;
+			document.getElementById("player-song").innerHTML = data.item.name;
+			document.getElementById("player-song").setAttribute("onclick", 'location.href=' + "'" + data.item.external_urls.spotify + "'");
+			
+			document.getElementById("player-artist").innerHTML = data.item.artists[0].name;
+			document.getElementById("player-artist").setAttribute("onclick", 'location.href=' + "'" + data.item.artists[0].external_urls.spotify + "'");
+
+			document.getElementById("player-status").innerHTML = data.is_playing
+				? `▶️ ${spotifyUser}'s now playing...`
+				: `⏸️ ${spotifyUser} has paused.`;
+
+			document.getElementById("player-album-art").setAttribute("src", data.item.album.images[1].url);
+			document
+				.getElementById("player-progress")
+				.setAttribute(
+					"style",
+					document.getElementById("player-progress").getAttribute("style") +
+					`width: ${(data.progress_ms * 100) / data.item.duration_ms}%`
+				);
+
+
+			progressSeconds = Math.ceil(songData.progress_ms / 1000);
+			totalSeconds = Math.ceil(songData.item.duration_ms / 1000);
+
+			
+			setProgress();
+			sleep(500);
+			updatePlayer();
+		});
+}
+
+function setProgress() {
+	if (progressSeconds > totalSeconds) {
+		clearInterval(progressInterval);
+
+		return;
+	}
+	++progressSeconds;
+	var totalLabel =
+		pad(parseInt(totalSeconds / 60)) + ":" + pad(totalSeconds % 60);
+	var progressLabel =
+		pad(parseInt(progressSeconds / 60)) + ":" + pad(progressSeconds % 60);
+	document.getElementById("player-time").innerHTML =
+		progressLabel + " / " + totalLabel;
+	document.getElementById("player-progress").style.width = `${(progressSeconds * 100) / totalSeconds
+		}%`;
+}
+
+function pad(val) {
+	var valString = val + "";
+	if (valString.length < 2) {
+		return "0" + valString;
+	} else {
+		return valString;
+	}
+}
+
+window.addEventListener("load", (event) => {
+	var d = document.getElementById("player");
+	d.className += " mloaded";
+});
+
+// Load player for the first time
+updatePlayer();
---
+++ worker.js
@@ -0,0 +1,129 @@
+
+addEventListener("fetch", (event) => {
+  var url = new URL(event.request.url);
+
+  // Simple routing
+  var route = url.pathname.replaceAll("/", "");
+  switch (route) {
+    case "hello":
+      event.respondWith(handleHello(event.request));
+
+      break;
+    case "authorize":
+      event.respondWith(handleAuthorization(event.request));
+
+      break;
+    case "callback":
+      event.respondWith(handleCallback(event.request));
+
+      break;
+    case "get-now-playing":
+      event.respondWith(handleNowPlaying(event.request));
+
+      break;
+    default:
+      event.respondWith(new Response("ERROR: Unsupported request."));
+  }
+});
+
+async function handleHello(request) {
+  return new Response("Hello!, This is my first middleware thingy.");
+}
+
+async function handleAuthorization(request) {
+  var state = "SPOTIFY_WIDGET"; // TODO: No use? remove?
+  var url = new URL(request.url);
+  var callback_url = `${url.protocol}//${url.hostname}/callback`;
+  var scope = "user-read-currently-playing";
+  var params = {
+    response_type: "code",
+    client_id: CLIENT_ID,
+    scope: scope,
+    redirect_uri: callback_url,
+    state: state,
+  };
+
+  // https://howchoo.com/javascript/how-to-turn-an-object-into-query-string-parameters-in-javascript
+  var queryString = Object.keys(params)
+    .map((key) => key + "=" + params[key])
+    .join("&");
+
+  return Response.redirect(
+    "https://accounts.spotify.com/authorize?" + queryString,
+    302
+  );
+}
+
+async function handleCallback(request) {
+  var url = new URL(request.url);
+  var callback_url = `${url.protocol}//${url.hostname}/callback`;
+  var code = url.searchParams.get("code") || null;
+  var state = url.searchParams.get("state") || null; // TODO: Might remove later?
+
+  if (state === "SPOTIFY_WIDGET") {
+    return fetch("https://accounts.spotify.com/api/token", {
+      method: "post",
+      headers: {
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `code=${code}&redirect_uri=${callback_url}&grant_type=authorization_code`,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else
+          return new Response("Something went wrong, retry authorization :/");
+      })
+      .then((data) => {
+        return new Response(`Your refresh token is: ${data.refresh_token}`);
+      });
+  } else return new Response("Something went wrong, retry authorization :/");
+}
+
+async function handleNowPlaying(request) {
+  // Get a new access token everytime :D << Easy way out (Without checking expire time)
+  var access_token = await fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    headers: {
+      Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=refresh_token&refresh_token=${REFRESH_TOKEN}`,
+  })
+    .then((response) => {
+      // TODO: Check errors here
+      return response.json();
+    })
+    .then((data) => {
+      return data.access_token;
+    });
+
+  var songData = await fetch(
+    "https://api.spotify.com/v1/me/player/currently-playing",
+    {
+      headers: {
+        Authorization: "Bearer " + access_token,
+      },
+    }
+  ).then((response) => {
+    // TODO: Check errors here
+    return response.text();
+  });
+
+  // https://mcculloughwebservices.com/2016/09/23/handling-a-null-response-from-an-api/
+  // If response is empty throw error
+  if (!songData) songData = { ERROR: "Couldn't retrieve now playing." };
+  else songData = JSON.parse(songData);
+
+  // Add CORS to allow requests from any domain
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Max-Age": "86400",
+  };
+
+  return new Response(JSON.stringify(songData, null, 2), {
+    headers: corsHeaders,
+  });
+}
